281bd0f1389eb7ad0f5fc0cee00421a9902981d9
TefiD <estefania.demitropulos@dinolcoudconsulting.com>
2020-03-25 14:45:12 -0300

    todoApp-phase3

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,46 +1,14 @@
 import React from "react"
-import Header from "./components/Header"
-import MainContent from "./components/MainContent"
-import Footer from "./components/Footer"
-import MyApp from "./components/MyApp"
-import MyInfo from "./components/MyInfo"
-import productsData from "./components/vsschoolProducts"
-import Product from "./components/Product"
+import Todoitem from "./components/Todoitem"
+import todosData from "./components/todosData"
+
 import "./style.css"
 
 function App(){
-    // const date = new Date();
-    // const hours = date.getHours();
-    // let timeOfDay;
-    // const styles= {
-    //     fontSize: "25px",
-    // }
-
-    // if(hours < 12){
-    //     timeOfDay= "morning"
-    //     styles.color = "#009dff"
-    // }else if(hours >= 12 && hours < 17){
-    //     timeOfDay= "afternoon"
-    //     styles.color = "#ff8000"
-    // }else{
-    //     timeOfDay= "night"
-    //     styles.color = "#13016b"
-    // }
-
-    const productComponents = productsData.map(item => <Product key={item.id} product={item}/>)
-
+    const todoItems = todosData.map(item => <Todoitem key={item.id} item={item}/>)
     return(
-        <div>
-            {/* <MyApp />
-            <MyInfo /> */}
-            <Header />
-            <MainContent />
-            {/* <Footer /> */}
-            {/* <h1 style={{color: "#ff8c00", backgroundColor: "#ff2d00"}}>Good {timeOfDay}!</h1> */}
-            {/* <h1 style={styles}>Good {timeOfDay}!</h1> */}
-            <div>
-                {productComponents}
-            </div>
+        <div className="todo-list">
+            {todoItems}
         </div>
     )
 }
---
+++ learning-react/src/components/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id:1,
+        text:"Take out the trash",
+        completed:true,
+    },
+    {
+        id:2,
+        text:"Grocery shopping",
+        completed:false,
+    },
+    {
+        id:3,
+        text:"Clean floors",
+        completed:false,
+    },
+    {
+        id:4,
+        text:"Go to work",
+        completed:true,
+    },
+    {
+        id:5,
+        text:"Play videogames",
+        completed:false,
+    },
+]
+
+export default todosData
