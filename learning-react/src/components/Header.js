ef04f585fd54c3bb9b1eeb20d5d616608e7a6b01
TefiD <estefania.demitropulos@dinolcoudconsulting.com>
2020-04-02 20:09:38 -0300

    meme generator app style added

--- learning-react/src/components/Header.js
+++ learning-react/src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function Header(){
     return(
-        <header>
+        <header className="header">
             <img
                 className="imgHeader" 
                 src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
